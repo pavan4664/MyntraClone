0201dd0cf2beba2128a4d3f925b34ee394e8d8b9
Pavan Kakde <pavankakde2006@gmail.com>
2026-01-30 16:16:44 +0530

    new code bag count aded

--- items.js
+++ items.js
@@ -1,54 +1,15 @@
-// let bagItems=[];
-
-// displyItems();
-// function  addToBag(itmesId)
-// {
-//     bagItems.push(itmesId );
-// } 
-
-
-
-
-
-
-
-
-
-// function displyItems(){
-//     let itemCont=document.querySelector('.items-container');
-//     let innerHTML='';
-//     items.forEach(items=>{
-//         innerHTML+=`
-//         <div class="item-container">
-//                 <img class="item-img"src="${items.image}" alt="item-image">
-//                 <div class="rating">
-//                     ${items.rating.stars} ★|${items.rating.count}
-//                 </div>
-//                 <div class="company-name">${items.company}</div>
-//                 <div class="item-name">${items.item_name}</div>
-//                 <div class="price">
-//                     <span class="Current-Price">Rs.${items.current_Price}</span>
-//                     <span class="Orignal">Rs.${items.original_price}</span> 
-//                     <span class="discount">${items.discount_percentage}% OFF</span>
-//                 </div>
-//                 <button class="btn-add-bag" onclick="addTag() ${items.id}">Add to Bag </button>
-//             </div>`
-
-
-
-// });
-// itemCont.innerHTML=innerHTML;
-
-
-
-
-// }
-
-
 
 let bagItems = [];
+onLoad();
 
-displayItems();
+function onLoad(){
+  displayItems();
+  displyaIcon();
+
+
+}
+
+
 
 function addToBag(itemId) {
   bagItems.push(itemId);
@@ -58,10 +19,12 @@ function addToBag(itemId) {
 function displyaIcon(){
 
   let bag=document.querySelector(".bag-count")
-  if(bagItems.length>0 && bagItems.innerHTML!=bag.innerHTML){
-  bag.innerHTML=bagItems.length;}
+  if(bagItems.length>0){
+    bag.style.visibility='visible';
+   bag.innerHTML=bagItems.length;
+  }
   else{
-    bagItems.style.visibility='hidden';
+    bag.style.visibility='hidden';
   }
 }
 
@@ -91,7 +54,7 @@ function displayItems() {
           Add to Bag
         </button>
       </div>
-    `;
+    `
   });
 
   itemCont.innerHTML = innerHTML;
